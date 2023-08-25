da6beed067c2d3af293bcad195dd9e64fa041c05
IuryFF96 <IFF_96@hotmail.com>
2023-08-25 15:40:14 -0300

    Usando data anotation

--- src/Tarefas.Web/Models/TarefaViewModel.cs
+++ src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -4,11 +4,16 @@ namespace Tarefas.Web.Models
 {
     public class TarefaViewModel
     {
+        [DisplayName("Id")]
         public int Id { get; set; }
-        [DisplayName("Descrição")]
+
+        [DisplayName("Título")]
         public string Titulo { get; set; }
+
+        [DisplayName("Descrição")]
         public string Descricao { get; set; }
-        [DisplayName("Títulos")]
+
+        [DisplayName("Status")]
         public string Status { get; set; }
     } 
 }   
